527a4b73439d314c22ff674e2c67cc38ee2b454e
Prakash Kumar <psingh5293@gmail.com>
2019-08-13 00:44:01 +0530

    compressing image and making them responsive

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,13 +1,14 @@
 import React, { FunctionComponent } from "react";
 import { IImageProps } from "../Interfaces/UserInterface";
+import { url } from "inspector";
 
 const Image: FunctionComponent<IImageProps> = ({ ImageUrl }) => {
   return (
-    <div>
+    <div style={{ height: "100%" }}>
       <img
         src={ImageUrl}
         alt="slide show images"
-        style={{ height: "80%", marginBottom: "30px" }}
+        style={{ maxWidth: "100%", maxHeight: "100%" }}
       />
     </div>
   );
